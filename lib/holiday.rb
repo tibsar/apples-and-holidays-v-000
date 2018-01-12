1a1e836bfddd70ae6552363dffe3620d438534b7
Sara Tibbetts <sxt4866@rit.edu>
2018-01-12 19:22:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].collect do |holiday, supplies|
+  test_array = holiday_hash[:winter].collect do |holiday, supplies|
     supplies
   end
 end
