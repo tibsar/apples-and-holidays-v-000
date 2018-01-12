ed8e74f3e04a8bd254876deab89685b94d6a660d
Sara Tibbetts <sxt4866@rit.edu>
2018-01-12 19:07:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ require 'pry'
 # return the second element in the 4th of July array
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash[:summer][:fourth_of_july]
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
