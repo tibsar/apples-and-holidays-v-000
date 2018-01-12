8534f99728ac71698f47fb612ae9b8a16e2e89e8
Sara Tibbetts <sxt4866@rit.edu>
2018-01-12 19:31:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts season.to_s.capitalize
+    puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
 
     end
