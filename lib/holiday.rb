c8963f52c87b8688ccda6ae1cd6ae44563488c64
Sara Tibbetts <sxt4866@rit.edu>
2018-01-12 19:36:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split("_").join("").capitalize}: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split("_").join(" ").capitalize}: #{supplies.join(", ")}"
     end
   end
 end
