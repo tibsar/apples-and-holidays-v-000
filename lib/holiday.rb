2aa2566d69309f3dbf14fd4e1e017ffaa234d69d
Sara Tibbetts <sxt4866@rit.edu>
2018-01-12 19:41:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,4 +72,5 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
+  end 
 end
